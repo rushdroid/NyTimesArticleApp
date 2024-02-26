2da68234c142b9d35c5a23abc467294be093c47e
Rushabh Prajapati <rushabhprajapati20@gmail.com>
2024-02-26 14:10:40 +0530

    Remove the key from Constants.kt for security reason

--- app/src/main/java/com/example/nytimesapp/util/Constants.kt
+++ app/src/main/java/com/example/nytimesapp/util/Constants.kt
@@ -2,5 +2,5 @@ package com.example.nytimesapp.util
 
 object Constants {
     const val API_BASE_URL = "https://api.nytimes.com/svc/"
-    const val API_KEY = "lzxDAUo2Q0o1KAnqdVHQkwZVAeqY79OX"
+    const val API_KEY = "REPLACE_YOUR_API_KEY"
 }
